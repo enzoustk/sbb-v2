0ce15a9c007d421b40462379e72ba28d57d9d2d0
Enzo Araújo <enzo@strikerinvest.com>
2025-03-23 19:49:44 -0300

    create events to update function

--- utils.py
+++ utils.py
@@ -1,4 +1,16 @@
 def print_separator():
     print('\n' + '-' * 60)
 
+def events_to_update():
+    from api_requests.pull_data import fetch_live_events
+    from constants.file_params import MADE_BETS
+    live_events = fetch_live_events()
 
+    live_events_ids = {str(event['id']) for event in live_events}
+    MADE_BETS['event_id'] = MADE_BETS['event_id'].astype(str)
+
+    new_events = MADE_BETS['final_score'].isna()
+    new_events = new_events.drop_duplicates(subset=['event_id'])
+    new_events = new_events[~new_events['event_id'].isin(live_events_ids)]
+    
+    return new_events
