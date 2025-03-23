9e0bb348975e3731a1bb25ff6575c538a072315d
Enzo Araújo <enzo@strikerinvest.com>
2025-03-23 19:48:46 -0300

    Created MAX_HOT param

--- model/model_config.py
+++ model/model_config.py
@@ -1,3 +1,4 @@
-EV_THRESHOLD = 0.05
-HOT_THRESHOLD = 0.1
-HOT_TIPS_STEP = 0.05
+EV_THRESHOLD = 0.05 # EV mínimo para enviar uma aposta
+HOT_THRESHOLD = 0.1 # EV mínimo para exibir "⚠️ EV:"
+HOT_TIPS_STEP = 0.05 # A cada 0.05, adiciona um "🔥"
+MAX_HOT = 4 #Quantidade de "🔥"
