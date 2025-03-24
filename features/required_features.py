80789005126734569e7dac9453fe5fd151eb93fe
Enzo Araújo <enzo@strikerinvest.com>
2025-03-24 11:43:59 -0300

    deleted files

--- features/required_features.py
+++
@@ -1,8 +0,0 @@
-REQUIRED_FEATURES = [
-    'h2h_count', 'l1', 'l2', 'l3',
-    'mediana_3', 'std_3', 'media_3', 'moda_3',
-    'ewma_total_15', 'ewma_total_3',
-    'mediana_total', 'std_total', 'media_total', 'moda_total',
-    'day_of_week', 'hour_of_day', 'day_sin', 'day_cos', 'hour_sin', 'hour_cos',
-    'tempo_dia_zero'
-]
