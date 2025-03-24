4145e3c3acaafd2edc1e310e9723b534dcde453e
Enzo Araújo <enzo@strikerinvest.com>
2025-03-24 11:36:59 -0300

    Rename file
    Better structure

---
+++ constants/api.py
@@ -0,0 +1,13 @@
+API_TOKEN = "183604-pWN7flhoAsWGu8"
+
+LEAGUE_ID = 22614
+SPORT_ID = 1
+
+URLS = {
+    "inplay": "https://api.betsapi.com/v1/bet365/inplay",
+    "odds": "https://api.b365api.com/v2/event/odds",
+    "ended_events": "https://api.b365api.com/v3/events/ended",
+    "event": "https://api.b365api.com/v1/event/view"
+}
+
+
--- constants/api_params.py
+++
@@ -1,6 +0,0 @@
-INPLAY_URL = "https://api.betsapi.com/v1/bet365/inplay"
-ODDS_URL = "https://api.b365api.com/v2/event/odds"
-ENDED_EVENTS_URL = "https://api.b365api.com/v3/events/ended"
-API_TOKEN = "183604-pWN7flhoAsWGu8"
-LEAGUE_ID = 22614
-SPORT_ID = 1
