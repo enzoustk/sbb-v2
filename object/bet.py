b55d413fedbbeccca4b586625413e5bb4c9dac4e
Enzo Araújo <enzo@strikerinvest.com>
2025-03-25 18:58:59 -0300

    work in progress

--- object/bet.py
+++ object/bet.py
@@ -1,8 +1,9 @@
 class Bet:
+    
     """
     All Bet-related data, methods and attributes.
     TODO: Create a subset to bet exported in xlsx
-    TODO: Create a way to delete Bets that are no longer needed to be stored
+    TODO: Create a way to delete Bet Objetcts that are no longer needed to be stored
     """
 
     def __init__(self, event: dict):
@@ -53,9 +54,10 @@ class Bet:
 
         self.profit = None
         self.result = None
-        self.canceled = False
+        self.canceled = None
 
-        self.sent = False
+        self.message = None
+        self.sent = None
         self.message_id = None
         self.chat_id = None
         self.edited = False
@@ -64,188 +66,40 @@ class Bet:
         self.time_interval = None
 
         self.saved_on_excel = False
+        self.month = None
         self.totally_processed = None
       
-    def print_event_data(self) -> None:
-        """
-        Print the event data
-        """
-        print(f"League: {self.league}")
-        print(f"{self.home_player} ({self.home_team}) vs {self.away_player} ({self.away_team})")
-        print('-' * 20)
-        print(f"Line: {self.handicap}")
-        print(f"Lambda: {self.lambda_pred}")
-        print('-' * 20)
-        print(f"Over Probability: {self.prob_over*100:.2f}%")
-        print(f'Over Odd: {self.odd_over}')
-        print(f"Under Probability: {self.prob_over*100:.2f}%")
-        print(f'Under Odd: {self.odd_under}')
-        print('-' * 20)
-        print(f"Over EV: {self.ev_over*100:.2f}%")
-        print(f"Under EV: {self.ev_under*100:.2f}%")
-        print('-' * 60)
-        
-        if self.bet_type is not None: 
-            print(f'Aposta: {self.bet_type} {self.handicap} @{self.bet_odd}')
-            print(f'Minimum Line: {self.minimum_line} @{self.minimum_odd}')
-            print(f'Hot Tip: {self.hot_ev}')
-        if self.bet_type is None: 
-            print('Não há EV+ nessa partida')
+         
+    def find_ev(self, lambda_pred: float):
+        from model.config import EV_THRESHOLD
+        from model import calculate
 
-    def validate(self, bet_type: str | None) -> bool:
-        self.bet_type = bet_type
-        
-    def get_lambda_pred(self, lambda_pred: float):
+        """
+        Calculate bet probabilities using calculate.poisson
+        Calculate ev for possible bets using calculate.ev
+        If ev > EV_THRESHOLD -> Return bet_type and bet_data;
+        Else: return None;
+        """
+
         self.lambda_pred = lambda_pred
         
-    def calculate_probabilities(self) -> tuple[str, float, float, float] | None:
-        from model.config import EV_THRESHOLD
-
-        """
-        Calcular probabilidades e EV;
-        Retornar probabilidades e EV caso seja maior que o threshold;
-        Retornar None caso não seja maior que o threshold;
-        Imprimir dados do evento;
-        """
-        
-
-        self.prob_over, self.prob_under = self.poisson(self.lambda_pred, self.handicap)
-        self.ev_over = self.odd_over * self.prob_over - 1
-        self.ev_under = self.odd_under * self.prob_under -1
-
-        self.print_event_data()
+        self.prob_over, self.prob_under = calculate.poisson_goals(self.lambda_pred, self.handicap)
+        self.ev_over = calculate.ev(self.odd_over, self.prob_over)
+        self.ev_under = calculate.ev(self.odd_under, self.prob_under)
 
         if self.ev_over >= EV_THRESHOLD:
-            self.bet_type = 'over'
-            self.bet_odd = self.odd_over
-            self.bet_prob = self.prob_over
-            self.bet_ev = self.ev_over
+            self._get_bet_type('over')
         
         elif self.ev_under >= EV_THRESHOLD:
-            self.bet_type = 'under'
-            self.bet_odd = self.odd_under
-            self.bet_prob = self.prob_under
-            self.bet_ev = self.ev_under
+            self._get_bet_type('under')
         
-        else:
-            print('No EV+ Bet Found')
-            return None
+        self._print_bet_data()
 
-    def poisson(self) -> tuple[float, float]:
+    def handle_made_bet(self):
+        self._find_min_line()
+        self._generate_message()
+        self._send_message
 
-        from scipy.stats import poisson
-
-        """
-        Calcula a probabilidade dado um handicap.
-        TODO: Ajustar para possibilitar uso de outros mercados.
-        """
-        
-        if self.handicap % 1 == 0.5:  # Handicap terminado em 0.5
-            self.prob_over = 1 - poisson.cdf(int(self.handicap), self.lambda_pred)
-            self.prob_under = poisson.cdf(int(self.handicap), self.lambda_pred)
-
-        elif self.handicap % 1 == 0.0:  # Handicap inteiro
-            prob_over_raw = 1 - poisson.cdf(int(self.handicap), self.lambda_pred)
-            prob_under_raw = poisson.cdf(int(self.handicap) - 1, self.lambda_pred)
-            total = prob_over_raw + prob_under_raw
-            self.prob_over = prob_over_raw / total
-            self.prob_under = prob_under_raw / total
-
-        elif self.handicap % 1 in [0.25, 0.75]:  # Handicap terminado em 0.25 ou 0.75
-            lower = self.handicap - 0.25
-            upper = self.handicap + 0.25
-            prob_over_lower, prob_under_lower = poisson(self.lambda_pred, lower)
-            prob_over_upper, prob_under_upper = poisson(self.lambda_pred, upper)
-            self.prob_over = (prob_over_lower + prob_over_upper) / 2
-            self.prob_under = (prob_under_lower + prob_under_upper) / 2
-
-        else:
-            raise ValueError(f"Handicap inválido: {self.handicap}")
-        
-        return self.prob_over, self.prob_under
-
-    def calculate_minimum(self) -> tuple[float, float]:
-        
-        """
-        Calcular a odd EV == threshold para a linha atual,
-        Se a odd mínima for inferior a 1.75, 'piorar' a linha em 0.25
-        Repetir até que a odd mínima seja superior a 1.75
-        """
-
-        self.minimum_line = self.handicap
-
-        if self.bet_type.lower() == 'over': 
-            step = 0.25
-        elif self.bet_type.lower() == 'under': 
-            step = -0.25
-        else: 
-            raise ValueError(f"Tipo de aposta inválido: {self.bet_type}")
-
-        while True:
-            from model.config import EV_THRESHOLD
-            self.prob_over, self.prob_under = self.poisson()
-
-            if self.bet_type.lower() == 'over': self.prob = self.prob_over
-            elif self.bet_type.lower() == 'under': self.prob = self.prob_under
-            else: raise ValueError(f"Tipo de aposta inválido: {self.bet_type}")
-
-            self.minimum_odd = ((1.0 + EV_THRESHOLD) / self.bet_prob).round(2)
-
-            if self.minimum_odd >= 1.75:
-                return self.minimum_line, self.minimum_odd
-
-            elif self.minimum_odd < 1.75: self.minimum_line += step
-
-            else: raise ValueError(f"Odd mínima inválida: {self.minimum_odd}")
-
-    def calculate_profit(self) -> float:
-        
-        """
-        Calcular o PL dado um tipo de aposta, handicap, odd e resultado.
-        TODO: Permitir outros mercados.
-        TODO: Permitir stake variável.
-        """
-
-        if self.bet_type.lower() == 'over': outcome = self.total_score - self.handicap
-        elif self.bet_type.lower() == 'under': outcome = self.handicap - self.total_score
-        
-        else:
-            raise ValueError("Tipo de aposta deve ser 'over' ou 'under'")
-
-        if outcome >= 0.5: 
-            self.profit = (self.bet_odd - 1)
-            self.result = 'win' 
-        elif outcome == 0.25: 
-            self.profit = (self.bet_odd - 1) / 2
-            self.result = 'half_win'
-        elif outcome == 0: 
-            self.profit = 0
-            self.result = 'push'
-        elif outcome == -0.25:
-            self.profit = -0.5 
-            self.result = 'half_loss'
-        elif outcome <= -0.5: 
-            self.profit -1
-            self.result = 'loss'
-        
-        else: 
-            raise ValueError(f"Ajuste de resultado inválido: {outcome}")
-    
-    def generate_and_send_message(self):
-        from constants.telegram_params import TELEGRAM_MESSAGE
-        from telegram import message
-        
-        self.message = TELEGRAM_MESSAGE.format(**self.__dict__)
-        self.implement_minimum_in_message()
-        self.implement_hot_tips()
-
-        if self.bet_type is not None:
-            self.message_id, self.chat_id = message.send(self.message)
-        
-        if self.message_id is not None:
-            self.sent = True
-            self._get_time_atributes()
-  
     def save_bet(self):
         import json, threading
         from files.paths import NOT_ENDED
@@ -261,9 +115,9 @@ class Bet:
         with open(NOT_ENDED, 'w') as file:
             json.dump(data, file, indent=4)
 
-    def process_not_ended(self):
+    def handle_not_ended_events(self):
         import json, logging, pandas as pd, threading
-        from files.paths import MADE_BETS, ALL_DATA, NOT_ENDED
+        from files.paths import ALL_DATA, NOT_ENDED
        
         not_ended, ended, error_events = [], [], []
 
@@ -282,15 +136,15 @@ class Bet:
                 Se não acabou
                     Pular
                 """
-                for bet_entry in data:
+                for _ in data:
                     bet = Bet()
                     bet.__dict__.update(bet)
                     bet._get_end()
 
                     if  bet.ended:
                         bet.finish_processing()
-                        bet.save_made_bet()
-                        bet.process()
+                        bet._save_made_bet()
+                        bet._mark_processed()
 
                     if bet.totally_processed == True: ended.append(bet)
                     elif bet.totally_processed == False: error_events.append(bet)
@@ -308,7 +162,7 @@ class Bet:
         except (FileNotFoundError, json.JSONDecodeError):
             logging.error(f"Error loading data from {NOT_ENDED}")
         
-    def save_made_bet(self):
+    def _save_made_bet(self):
         from files.paths import MADE_BETS
         import pandas as pd
 
@@ -334,31 +188,22 @@ class Bet:
                 new_data.to_excel(writer, sheet_name=str(self.month), index=False)
                 
     def finish_processing(self):
+                from model import calculate
                 """
                 Calcula tudo o que falta.
                 Se alguma coisa der errado, marca totally_processed como false.
                 TODO: Colocar try except em todos os métodos abaixo
                 """
                 if self.bet_type is not None:
-                    self.calculate_profit()
-                    self.edit_telegram_message()
+                    self.profit, self.result = calculate.profit(self.bet_type, self.handicap, self.total_score, self.bet_odd)
+                    self._edit_telegram_message()
+                    self._save_made_bet()
+                             
 
-    def process(self):
-        if self.bet_type is not None:
-            self.totally_processed = all([
-                self.sent,
-                self.edited,
-                self.ended,
-                self.saved_on_excel,
-                self.profit is not None,
-                self.result is not None,
-                self.raw_score is not None
-                ])
-        else: 
-            if self.raw_score is not None:
-                self.totally_processed = True
-                                
-    def edit_telegram_message(self):
+
+    # ------------------------------------------- #
+
+    def _edit_telegram_message(self):
         from constants.telegram_params import EDITED_MESSAGE
         from telegram.message import edit
         '''
@@ -369,6 +214,14 @@ class Bet:
         self.message += EDITED_MESSAGE.format(**self.__dict__)
         self._escape()
         self.edited = edit(self.message_id, self.message, self.chat_id)
+
+    def _get_bet_type(self, bet_type: str | None):
+        """
+        Gets Bet Type Object.
+        Possible Types: 'over', 'under', None
+        """
+
+        self.bet_type = bet_type
 
     def _escape(self):
         self.message = (
@@ -508,36 +361,23 @@ class Bet:
         
         self.home_score, self.away_score = map(int, self.raw_score.split('-'))
         self.total_score = self.home_score + self.away_score
-    
-    def _implement_minimum_in_message(self):
-        from constants.telegram_params import MIN_LINE_MESSAGE,MIN_ODD_MESSAGE
 
-        if self.minimum_line == self.handicap and self.minimum_odd != self.bet_odd:
-            self.message += MIN_ODD_MESSAGE.format(**self.__dict__)
+    def _get_hot_tip(self):
+        from model.config import (HOT_THRESHOLD, HOT_TIPS_STEP, MAX_HOT)
         
-        elif self.minimum_line != self.handicap:
-            self.message += MIN_LINE_MESSAGE.format(**self.__dict__)
-
-    def _implement_hot_tips(self):
-        from model.config import (HOT_THRESHOLD, HOT_TIPS_STEP, MAX_HOT, HOT_TIPS_MESSAGE)
-        
-        _ev = self.bet_ev
-        self.hot_ev = 0
-        
-        if _ev >= HOT_THRESHOLD: 
+        if self.bet_ev >= HOT_THRESHOLD:
+            _ev = self.bet_ev
+            self.hot_ev = 0
+            
             while True:
+
+                if self.hot_ev == MAX_HOT:
+                    break 
+
                 if _ev >= HOT_THRESHOLD:
                     self.hot_emoji += "🔥"
                     _ev -= HOT_TIPS_STEP
                     self.hot_ev += 1
-                
-                if self.hot_ev == MAX_HOT: 
-                    self.message += HOT_TIPS_MESSAGE.format(**self.__dict__)
-                    break
-                
-                else:
-                    self.message += HOT_TIPS_MESSAGE.format(**self.__dict__)
-                    break
 
     def _get_time_atributes(self):
         import pandas as pd
@@ -560,6 +400,7 @@ class Bet:
             'Liga': self.league,
             'Partida': self.home_str + 'vs. ' + self.away_str,
             'Tipo Aposta': self.bet_type.capitalize(),
+            'Hot Tips': self.hot_emoji,
             'Linha': f'{self.line:2f}',
             'Resultado' : self.result.replace('_', ' ').capitalize(),
             'Odd': f'{self.odds:2f}',
@@ -567,5 +408,108 @@ class Bet:
             'Intervalo': self.time_range,
         }
 
+    def _get_lambda_pred(self, lambda_pred: float):
+        self.lambda_pred = lambda_pred
+     
+    def _get_bet_type(self, bet_type: str | None = None):
+        
+        self.bet_type = bet_type
+
+        if self.bet_type is 'over':
+            self.bet_odd = self.odd_over
+            self.bet_prob = self.prob_over
+            self.bet_ev = self.ev_over
+        
+        elif self.bet_type is 'under':
+            self.bet_odd = self.odd_under
+            self.bet_prob = self.prob_under
+            self.bet_ev = self.ev_under
+    
+    def _print_bet_data(self):
+        """
+        Print the event data;
+        Print Betting data, such as EV, Probabilities and Bet.
+        """
+        print(f"League: {self.league}")
+        print(f"{self.home_player} ({self.home_team}) vs {self.away_player} ({self.away_team})")
+        print('-' * 20)
+        print(f"Line: {self.handicap}")
+        print(f"Lambda: {self.lambda_pred}")
+        print('-' * 20)
+        print(f"Over Probability: {self.prob_over*100:.2f}%")
+        print(f'Over Odd: {self.odd_over}')
+        print(f"Under Probability: {self.prob_over*100:.2f}%")
+        print(f'Under Odd: {self.odd_under}')
+        print('-' * 20)
+        print(f"Over EV: {self.ev_over*100:.2f}%")
+        print(f"Under EV: {self.ev_under*100:.2f}%")
+        print('-' * 60)
+        
+        if self.bet_type is not None: 
+            print(f'Bet: {self.bet_type} {self.handicap} @{self.bet_odd}')
+            print(f'Minimum Line: {self.minimum_line} @{self.minimum_odd}')
+            print(f'Hot Tip: {self.hot_ev}')
+        
+        if self.bet_type is None: 
+            print('No EV+ Bet Found')
+  
+    def _find_min_line(self):
+        from model import calculate
+        self.minimum_line, self.minimum_odd = calculate.min_goal_line(self.handicap, self.bet_type, self.bet_prob)
+
+    def _generate_message(self):
+        
+        from constants.telegram_params import TELEGRAM_MESSAGE, MIN_LINE_MESSAGE, MIN_ODD_MESSAGE, HOT_TIPS_MESSAGE
+        
+        """"
+        If bet_type is none, breaks
+        Creates and formats telegram message based
+        on current object bet data
+            if bet_type == None, break
+        Looks at minimum line and hot tips data.
+        If it exists, appends to the message.
+        """
+        
+        self.message = TELEGRAM_MESSAGE.format(**self.__dict__)
+
+        if self.minimum_line == self.handicap and self.minimum_odd != self.bet_odd:
+            self.message += MIN_ODD_MESSAGE.format(**self.__dict__)
+        
+        elif self.minimum_line != self.handicap:
+            self.message += MIN_LINE_MESSAGE.format(**self.__dict__)
+        
+        if self._get_hot_tip() is not None:
+            self.message += HOT_TIPS_MESSAGE.format(**self.__dict__)
+
+    def _send_message(self):
+        '''
+        If bet time is not None, gets called
+        Sends the message on telegram using message.send
+        Marks self.sent as True or False, depending if it sent sucessfully
+        Gets all time-related class objects
+        '''
+
+        from telegram import message
+        
+        self.message_id, self.chat_id = message.send(self.message)
+        if self.message_id is None:
+            self.sent = False
+            self._get_time_atributes()
+
+    def _mark_processed(self):
+        if self.bet_type is not None:
+            self.totally_processed = all([
+                self.sent,
+                self.edited,
+                self.ended,
+                self.saved_on_excel,
+                self.profit is not None,
+                self.result is not None,
+                self.raw_score is not None
+                ])
+        else: 
+            if self.raw_score is not None:
+                self.totally_processed = True
+   
     def cancel(self):
         pass
