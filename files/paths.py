c1c09884033e1a99ca33e95689e1c1b69d851425
Enzo Araújo <enzo@strikerinvest.com>
2025-03-25 07:40:51 -0300

    Improved paths explanation

--- files/paths.py
+++ files/paths.py
@@ -1,3 +1,11 @@
-GAME_DATA = 'data\sheets\game_data.csv'
+# Data used to calculate features and train model
+HISTORIC_DATA = 'data\sheets\game_data.csv'
+
+#Events with error
 ERROR_EVENTS = 'data\sheets\error_events.csv'
+
+#All matches we betted on
 MADE_BETS = r'data\sheets\bets.xlsx'
+
+# All analised matches
+GAME_DATA = r'data\sheets\all_data.csv'
