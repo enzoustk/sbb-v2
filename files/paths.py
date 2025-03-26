34deb4783a20a1f4ebabfa6e6fe7b3f676bb23e2
Enzo Araújo <enzo@strikerinvest.com>
2025-03-26 14:39:53 -0300

    Updated paths

--- files/paths.py
+++ files/paths.py
@@ -1,12 +1,10 @@
 # Data used to calculate features and train model
 HISTORIC_DATA = 'data\sheets\game_data.csv'
 
-#Events with error
-ERROR_EVENTS = 'data\sheets\error_events.csv'
-
 #All matches we betted on
 MADE_BETS = r'data\sheets\bets.xlsx'
 
 # All analised matches
 ALL_DATA = r'data\sheets\all_data.json'
 NOT_ENDED = r'data\sheets\not_ended.json'
+ERROR_EVENTS = 'data\sheets\error_events.json'
