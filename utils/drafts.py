2532ecbaaff0dd960fb99060d9780b161f3a15e7
Enzo Araújo <enzo@strikerinvest.com>
2025-04-03 12:27:09 -0300

    Continue finishing reports method

--- utils/drafts.py
+++ utils/drafts.py
@@ -1,29 +1,41 @@
-from telegram.constants import REPORT_TITLE
 import pandas as pd
+from telegram.constants import REPORT_TOTAL, REPORT_TIME_RANGE_BODY, REPORT_TIME_RANGE_TITLE
+from model.config import TIME_RANGES
 
-def generate_title(self, df: pd.DataFrame):
-    title = {}
-    title['interval'] = self.interval
-    title['league'] = _get_league(df)
-    title['ev_type'] = _get_ev_type
-    title['period_type'] = _get_period_type
 
-    message += REPORT_TITLE.format(title)
+def generate_total(self,
+    df: pd.DataFrame,
+    message: str = ''
+    ) -> str:
 
-def _get_league(self, df: pd.DataFrame):
-    """
-    Se só tem uma liga, retorna essa liga.
-    Se tem mais de uma, retorna um str: 'Liga 1,
-    Liga 2, Liga 3, ...
-    """
+
+    profit = df['profit'].sum()
+    total_emoji = self.get_emoji(profit)
+    vol = df['profit'].count()
+    roi = profit / vol * 100 if vol > 0 else 0
+    players_df = _get_players_df(df)
+    notable_players = _get_notable_players(players_df)
+
+        
+
+
+def _get_notable_players(self,
+    df: pd.DataFrame
+    ) -> dict:
     
-    leagues = df['league'].drop_duplicates().tolist
-    
-    if len(leagues) == 1:
-        return str(leagues)
-    
-    else:
+    notable_players = {}
+
+    if not df.empty and 'profit' in df.columns:
+        
+        bp_row = df.loc[df['profit'].idxmax()]
+        notable_players['best_player'] = bp_row.to_dict()
+        notable_players['best_player']['emoji'] = _get_emoji(bp_row['profit'])
+        
+        wp_row = df.loc[df['profit'].idxmin()]
+        notable_players['worst_player'] = wp_row.to_dict()
+        notable_players['wors_player']['emoji'] = _get_emoji(wp_row['profit'])
+
         
     
-
-
+    return notable_players
+   
