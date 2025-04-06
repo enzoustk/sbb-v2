58d916ae9be3c52e452ac1385b94ce2a959d02aa
Enzo Araújo <enzo@strikerinvest.com>
2025-04-06 10:19:19 -0300

    Updated main

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 import logging
+import threading
 from data import update
-from threads import threads
+from processes import scanner
 
 
-"""
-Thread 1:   Scanner no Pau o tempo todo
-
-Thread 2:   0. |Ao ligar o código: fill_data_gaps;
-            1. |Lê JSON not_ended;
-                1.1 |Se tiver jogo com aposta feita, time.sleep (60s):
-                        1.1.1 | Ficar procurando placar p/ Processar;
-                1.2 | Else: tenta processar todos os not_ended sem aposta
-
-Thread 3:   Envia o Relatório todo dia 00:00
-
-Thread 4?   Retreinar o Modelo
-"""
-def main():
+if __name__ == '__main__':
     """
     When Started, the code will:
     1- Fill the data gaps;  
@@ -28,18 +16,7 @@ def main():
     
     logging.info('Starting updater...')
     logging.info('Filling data gaps...')
+    
     update.fill_data_gaps()
 
-    threads.scanner(model='', i=500)
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
+    scanner.run(model='', i=50)
