963c522d20189a97faf0b94592eb35f82345143d
Enzo Araújo <enzo@strikerinvest.com>
2025-04-07 09:41:40 -0300

    Added logging config

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import threading
+from datetime import datetime
 from data import update
 from processes import scanner
 
@@ -13,6 +14,13 @@ if __name__ == '__main__':
     4- If predict function finds a new event, it will call the updater function;
 
     """
+    now = datetime.now().strftime("%d-%m %H%M%S")
+    logging.basicConfig(
+        level=logging.WARNING,
+        filename=f'{now}.log',
+        filemode='w',
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
     
     logging.info('Starting updater...')
     logging.info('Filling data gaps...')
