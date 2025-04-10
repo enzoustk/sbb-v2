33356b5ae4886380681dbdc25eca0357ff5031be
Enzo Araújo <enzo@strikerinvest.com>
2025-04-10 16:33:33 -0300

    Rename all module

--- support_bot/constants.py
+++ support_bot/constants.py
@@ -3,7 +3,7 @@
 FILE_PATH_DADOS = 'a'
 CHAT_ID = 'b'
 
-BOT_TOKEN_PARCIAL = "7709297039:AAFTRjYDzfZZ9yYL0XWwbtcCoboeLWqGagY"
-USERS_PERMITIDOS = [6045850973, 987654321]  # Substitua pelos IDs reais dos usuários
+SUPPORT_BOT_TOKEN = "7709297039:AAFTRjYDzfZZ9yYL0XWwbtcCoboeLWqGagY"
+USERS_ALLOWED = [6045850973, 987654321]  # Substitua pelos IDs reais dos usuários
 CHAT_FORMULARIO = -1002343941988  # Substitua pelo ID do grupo no Telegram
 
