3eedf5ebf3ee6decdc09b28333ebabfe19db8d44
Enzo Araújo <enzo@strikerinvest.com>
2025-04-10 11:16:43 -0300

    Updated Required Features

--- features/required.py
+++ features/required.py
@@ -4,11 +4,12 @@ REQUIRED_FEATURES = [
     
     # Time-based features
     'h2h_count', 'last_h2h', 'time_since_start',
-    'day_of_week', 'hour_of_day', 'day_angle', 'hour_angle',
+    'day_angle', 'hour_angle',
 
     # Goal-based features
     'l1', 'l2', 'l3', 
     'median', 'std', 'avg'
     'median_3', 'std_3', 'avg_3',
     'ewma_total_15', 'ewma_total_3',
+    'max_5', 'min_5', 'delta_5'
 ]
