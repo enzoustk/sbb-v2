a630dd143a9092d75a359c6d60df9e1cbc2c7727
Enzo Araújo <enzo@strikerinvest.com>
2025-04-20 16:14:27 -0300

    Adjusted logger and error treatment

--- data/update.py
+++ data/update.py
@@ -7,49 +7,44 @@ from api import fetch
 from model.betting_config import AJUSTE_FUSO
 from files.paths import HISTORIC_DATA, NOT_ENDED, ERROR_EVENTS, LOCK
 
+logger = logging.getLogger(__name__)
+
 
 def historic_data(data: list):
     """Update HISTORIC_DATA.csv with new data."""
 
-
     if not data:
-        logging.debug(f'No new ended events')
+        logger.debug('No new ended events')
         return
     
     existing_data = load.data('historic')
     existing_data['date'] = pd.to_datetime(existing_data['date'])
+    existing_data = existing_data.dropna(axis=1, how='all')
     exclude_keys = remove_columns_to_historic()
     
     new_data = [
-        {key: value for key,
-        value in bet.__dict__.items()
-        if key not in exclude_keys}  
+        {key: value for key, value in bet.__dict__.items() if key not in exclude_keys}
         for bet in data
     ]
 
     if not new_data:
-        logging.info('No data to update')
+        logger.info('No data to update')
         return
     
     new_df = pd.DataFrame(new_data)
     new_df['date'] = pd.to_datetime(new_df['date'])
     new_df = new_df.sort_values(by='date')
-
+    new_df = new_df.dropna(axis=1, how='all')
 
     if existing_data.empty:
-        logging.debug('Existing historic data is empty, creating new file...')
+        logger.debug('Existing historic data is empty, creating new file...')
         new_df.to_csv(HISTORIC_DATA, index=False)
-        logging.info(f'Historic file created.'
-            f'{len(new_df)} matches added.')
-        
-
+        logger.info(f'Historic file created. {len(new_df)} matches added.')
     else:
-        combined_df: pd.DataFrame = pd.concat([existing_data, new_df])
-        combined_df['date'] = pd.to_datetime(combined_df['date'])
-        combined_df.drop_duplicates(subset='event_id')
+        combined_df = pd.concat([existing_data, new_df])
+        combined_df = combined_df.drop_duplicates(subset='event_id', keep='first')
         combined_df.to_csv(HISTORIC_DATA, index=False)
-
-        logging.info(f'{len(new_df)} new matches added to ended events')
+        logger.info(f'{len(new_df)} new matches added to ended events.')
         
 
 def fill_data_gaps(gap: int = 30):
@@ -60,10 +55,10 @@ def fill_data_gaps(gap: int = 30):
         existing_data = load.data('historic')
 
         if existing_data.empty or 'date' not in existing_data.columns:
-            logging.warning("DataFrame 'existing_data' is empty. skipping data gaps fill.")
+            logger.warning("DataFrame 'existing_data' is empty. skipping data gaps fill.")
             return 
 
-        logging.info('Coverting time column to datetime')
+        logger.info('Coverting time column to datetime')
         existing_data['date'] = pd.to_datetime(existing_data['date']) + pd.Timedelta(hours=AJUSTE_FUSO)
         existing_data['date_day'] = existing_data['date'].dt.normalize()
         
@@ -82,7 +77,7 @@ def fill_data_gaps(gap: int = 30):
             processed_dates.add(date)
 
         formatted_dates = "\n".join(date.strftime("%d-%m-%y") for date in processed_dates)
-        logging.info(f"Dates to fetch:\n{formatted_dates}")
+        logger.info(f"Dates to fetch:\n{formatted_dates}")
 
         
         matches_fetched = fetch.events_for_date(
@@ -110,19 +105,19 @@ def fill_data_gaps(gap: int = 30):
             final_df = final_df.sort_values(by='date')
 
             final_df.to_csv(HISTORIC_DATA, mode='w', index=False)
-            logging.info(f'{len(new_events)} novos eventos adicionados ao histórico.')
+            logger.info(f'{len(new_events)} novos eventos adicionados ao histórico.')
 
 
 def not_ended(data: list):
     """Updates NOT_ENDED.csv with new data."""
     if not data:
         pd.DataFrame().to_csv(NOT_ENDED, index=False)
-        logging.info('All not ended data fully processed')
+        logger.info('All not ended data fully processed')
         return
     
     ne_df = pd.DataFrame([bet.__dict__ for bet in data])
     ne_df.to_csv(NOT_ENDED, index=False)
-    logging.info(
+    logger.info(
         'Not_ended events updated.'
         f' {len(ne_df)} left to update.'
         )
@@ -137,9 +132,8 @@ def error_events(data: list):
     
     error_df.to_csv(ERROR_EVENTS, mode='a', index=False, header=header)
     
-    if error_df is not None:
-        logging.info(f' Added {len(error_df)} error events to error file')
-        logging.info(f" Events added to Error Events: {error_df}")
+    if len(error_df > 0):
+        logger.info(f' Added {len(error_df)} error events to error file')
 
 
 def remove_columns_to_historic():
