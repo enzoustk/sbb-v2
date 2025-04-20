e7e478273ab21521a1ba92da0a9f634ecf6539af
Enzo Araújo <enzo@strikerinvest.com>
2025-04-19 23:06:48 -0300

    Update constants.py

--- support_bot/constants.py
+++ support_bot/constants.py
@@ -1,10 +1,1 @@
-
-
-FILE_PATH_DADOS = 'a'
-CHAT_ID = 'b'
-
 SUPPORT_BOT_TOKEN = "7709297039:AAFTRjYDzfZZ9yYL0XWwbtcCoboeLWqGagY"
-USERS_ALLOWED = [<CHAT_ID>, 987654321]  # Substitua pelos IDs reais dos usuários
-CHAT_FORMULARIO = -1002343941988  # Substitua pelo ID do grupo no Telegram
-
-OLLAMA_MODEL = 'deepseek-r1:7b'
