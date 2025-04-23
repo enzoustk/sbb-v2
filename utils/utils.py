ac710c3f04302646a3d54eff8d269d4eb25eff3f
Enzo Araújo <enzo@strikerinvest.com>
2025-04-23 11:27:05 -0300

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,17 @@
+import pandas as pd
+
 def print_separator(size: int = 60):
     print('\n' + '-' * size + '\n')
 
+def print_features(features: pd.DataFrame):
+    chunk = 3
+    i = 0 
+    output = [f'\n']
+    for key, value in features.iloc[0].items():
+            output.append(f'{key}: {value:.2f}')
+            i += 1
+            if i % chunk == 0:
+                output.append('\n')
+            else:
+                output.append(', ')
+    return ''.join(output).strip()
